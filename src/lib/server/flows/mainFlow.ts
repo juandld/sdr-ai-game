2ae90ad81163398ce3a7582ceeb9a1b120dc6fef
juandld <juanda.lopez3011@gmail.com>
2024-04-13 19:38:03 -0500

    base langchain structure set, just missing flow

--- src/lib/server/flows/mainFlow.ts
+++ src/lib/server/flows/mainFlow.ts
@@ -7,7 +7,7 @@ import { conversationTemplates } from "../prompts/mainPrompts";
 export const starterConvo = async (data: any) => {
     // Generate chatHistory in Lanchain format for main chat injection.
     const { chatTemplate, bufferMemory } = await genBufferMemory(data.character, data.chat);
-    
+
     // Generate main chat callable llm
     const mainChain = await genMainChain(chatTemplate, bufferMemory);
     // Generate calls with core templates
@@ -15,21 +15,21 @@ export const starterConvo = async (data: any) => {
 
     const latestHMessage = data.chat[data.chat.length - 1].content;
     // Fallback
-    let response;
+    let response = "Sorry, I cant hear you, maybe try again later?";
 
     const [coherenceRaw, stageRaw] = await Promise.all([
         // Veriify chat coherence to make sure human is not messing with AI
         decisionCalls['resolveCohesion']({ chatHistory: JSON.stringify(data.chat) }),
-         // Verify chat stage, to resolve progression and also further coherence down the line
+        // Verify chat stage, to resolve progression and also further coherence down the line
         decisionCalls['identifyCurrentPhase']({ chatHistory: JSON.stringify(data.chat) }),
     ]);
 
     const coherence = Number(coherenceRaw.match(/\d+/)[0]);
-    const stage = Number(stageRaw.match(/\d+/)[0]);   
+    const stage = Number(stageRaw.match(/\d+/)[0]);
 
-    if (coherence == 1){
-        response = await mainChain.invoke({input: latestHMessage});
-    }else {
+    if (coherence == 1) {
+        response = await mainChain.invoke({ input: latestHMessage });
+    } else {
         //If cohesion good and stage is 1, invoke main chat, if not respond "I´m sorry, what?"
         response = "I'm sorry, what?"
     }
@@ -41,8 +41,7 @@ export const starterConvo = async (data: any) => {
     //If evaluation is good enough, decide to continue to next stage
 
     //Make final evaluation
-    console.log(response);
-    
+
     return response
 }
 
--- src/lib/server/util/bufferMemory.ts
+++ src/lib/server/util/bufferMemory.ts
@@ -13,16 +13,18 @@ interface ChatMessage {
 // Generalsystem template that is filled with character info.
 const systemMessageTemplate = SystemMessagePromptTemplate.fromTemplate(`You are {fullName},
     a {age}-year-old {role} of {company} from {location} You have just received an unexpected
-    call from an unknown number {circumstance}, DO NOT BREAK CHARACTER FOR ANY REASON, DO NOT ANSWER UNRELATED QUESTIONS`
+    call from an unknown number {circumstance}, DO NOT BREAK CHARACTER FOR ANY REASON, DO NOT ANSWER UNRELATED QUESTIONS, DO NOT ADD ANY UNSOLICITED UNASKED FOR INFORMATION, DO NOT MAKE ANY INFORMATION UP, YOU KEEP RESPONSES CLEAR AND SHORT`
 )
 
 const bufferMemory = new BufferMemory({ returnMessages: true, memoryKey: "history"})
 
 export const genBufferMemory = async (character: Character, chat: ChatMessage[]) => {
-    // Resolve system template
+    // Resolve system template, it returns an array for some reason
     const inception = await systemMessageTemplate.invoke(character)
-    // Add system message to the chat history
-    bufferMemory.chatHistory.addMessage(new SystemMessage("you are Mary"));
+    console.log(inception);
+    
+    // Add system message to the chat history, only Basemessage in the array
+    bufferMemory.chatHistory.addMessage(inception[0]);
     //Template that is added to the chain
     const chatTemplate = ChatPromptTemplate.fromMessages([
         new MessagesPlaceholder("history"),
@@ -32,9 +34,9 @@ export const genBufferMemory = async (character: Character, chat: ChatMessage[])
     //Todo: I am going off of the chat stored in client, users could manipulate the chat to get a passing score or outcome. will have to verify in server...
     chat.forEach((message) => {
         if (message.tag === "Human") {
-            bufferMemory.chatHistory.addMessage(new HumanMessage(message.content));
+            bufferMemory.chatHistory.addMessage(new HumanMessage(JSON.stringify(message.content)));
         } else if (message.tag === "AI") {
-            bufferMemory.chatHistory.addMessage(new AIMessage(message.content));
+            bufferMemory.chatHistory.addMessage(new AIMessage(JSON.stringify(message.content)));
         }
     });
 
